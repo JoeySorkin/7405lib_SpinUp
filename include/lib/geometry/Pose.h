7ab1ece7d05c9f0dfcd104f5c3d2f1fbcaa789a9
Alex Wong <alexwong@Alexs-MacBook-Air-2.local>
2023-03-16 15:35:58 -0400

    POSE methods

--- include/lib/geometry/Pose.h
+++ include/lib/geometry/Pose.h
@@ -12,4 +12,14 @@ class Pose{
         double getY() const;
         double getTheta() const;
         double getShortTheta() const;
+        double distanceTo(Pose other) const;
+        double headingToPoint(Pose other) const;
+
+        Pose transformBy(double delta_x, double delta_y, double delta_theta) const;
+        Pose transformBy(Pose other) const;
+        // Pose exp?????
+        static Pose getTransformation(Pose &A, Pose &B);
+        Pose scale(double scalar) const;
+        
+
 };
